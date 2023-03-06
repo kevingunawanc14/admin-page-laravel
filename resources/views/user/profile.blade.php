3cbda17cd9a6003bd40efcfc500eda0152cdfd7c
kevingunawanc14 <c14200005@john.petra.ac.id>
2023-03-06 11:42:47 +0700

    Update profile.blade.php

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -8,13 +8,13 @@
     
   <div class="pagetitle">
     <h1>Profile</h1>
-    <nav>
+    {{-- <nav>
       <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
         <li class="breadcrumb-item">Users</li>
         <li class="breadcrumb-item active">Profile</li>
       </ol>
-    </nav>
+    </nav> --}}
   </div><!-- End Page Title -->
 
   <section class="section profile">
