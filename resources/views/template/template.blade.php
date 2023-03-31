9b70d7fce5271e5feddb9d951a51bd2476698b61
kevingunawanc14 <c14200005@john.petra.ac.id>
2023-03-31 22:13:03 +0700

    remove footer

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -58,7 +58,7 @@
     @yield('content')
 
     <!-- ======= Footer ======= -->
-    @include('bagian.footer')
+    {{-- @include('bagian.footer') --}}
 
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
