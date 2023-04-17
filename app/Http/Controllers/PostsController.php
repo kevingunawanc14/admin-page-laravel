7337ccb3c91ca352b0bd73700584aec3ccff4ae9
kevingunawanc14 <c14200005@john.petra.ac.id>
2023-04-17 10:55:04 +0700

    api

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Team;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+
+    public function getProduct(){
+        $posts = Product::select('*')->get();
+        return new PostResource(true,'List Data Product',$posts);
+    }
+
+    public function getTeam(){
+        $posts = Team::select('*')->get();
+        return new PostResource(true,'List Data Team',$posts);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
