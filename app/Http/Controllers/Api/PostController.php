a713c6b1da82e0d7d6d2c4ed38e0f510a97b4ad2
kevingunawanc14 <c14200005@john.petra.ac.id>
2023-04-20 22:32:38 +0700

    api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\Catalog;
 use App\Models\Product;
 use App\Models\Team;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 
 class PostController extends Controller
